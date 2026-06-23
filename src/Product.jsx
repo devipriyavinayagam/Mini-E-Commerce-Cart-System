ed61dfe8d42b04f1aebf7e627d685322ed33ff4b
devipriyavinayagam <devipriyavinayagam32@gmail.com>
2026-06-23 21:56:51 +0530

    Update Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FiSearch, FiShoppingCart, FiChevronRight } from 'react-icons/fi'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 import { useCart } from './CartContext.jsx'
 
 const Product = () => {
